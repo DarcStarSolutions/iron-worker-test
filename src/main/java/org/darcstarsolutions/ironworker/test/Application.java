8afc318be4ec01f29821487be18036fa4847eaf6
Michael Haris <mharris021@hotmail.com>
2015-05-26 17:56:06 -0500

    initial commit

---
+++ src/main/java/org/darcstarsolutions/ironworker/test/Application.java
@@ -0,0 +1,14 @@
+package org.darcstarsolutions.ironworker.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by mharris on 5/26/15.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
